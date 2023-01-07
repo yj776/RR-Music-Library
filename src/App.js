937caacdc2dd45707befba9d4caa68bace4969f4
yj776 <yj776@nyu.edu>
2023-01-07 13:19:52 -0500

    Refactor - changed to withContext branch; added context provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import {DataContext} from './context/DataContext'
 
 function App() {
   let [searchTerm, setSearchTerm] = useState('')
@@ -33,7 +34,9 @@ function App() {
     <div className="App">
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,10 @@
+import {useContext} from 'react'
+import {DataContext} from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
 function Gallery(props){
-
-    const display = props.data.map((item, index) => {
+    const data = useContext(DataContext)
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
