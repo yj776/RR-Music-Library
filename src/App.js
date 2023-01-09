8d3af8c0788267ca2594ccf3cc9012155f5313c9
Amii Jiang <amii@MacBook-Pro.fios-router.home>
2023-01-09 15:44:09 -0500

    Refactor PII - SearchBar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import {DataContext} from './context/DataContext'
+import {SearchContext} from './context/SearchContext'
 
 function App() {
   let [searchTerm, setSearchTerm] = useState('')
   let [data, setData] = useState([])
   let [message, setMessage] = useState('Search for Music!')
+  let searchInput = useRef('')
 
-  useEffect(() => {
-    if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
+  const API_URL = 'https://itunes.apple.com/search?term='
 
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearchTerm(term)
+    const fetchData = async () => {
+      document.title=`${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if(resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    fetchData()
   }
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
+      <SearchContext.Provider value = {{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
+        <SearchBar />
+      </SearchContext.Provider>
       {message}
       <DataContext.Provider value={data}>
         <Gallery />
