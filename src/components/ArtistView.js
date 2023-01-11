e9d58867b1515f3ebe291bde05691bc935f003a9
Amii Jiang <amii@MacBook-Pro.fios-router.home>
2023-01-11 17:26:07 -0500

    update

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -10,6 +10,19 @@ function ArtistView(){
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        console.log('API_URL: ', API_URL)
+        const fetchData = async () => {
+            console.log('fetchData')
+            const response = await fetch(API_URL)
+            console.log('response: ', response)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    },[id])
+
     return(
         <div>
             <h2> The id passed was: {id} </h2>
